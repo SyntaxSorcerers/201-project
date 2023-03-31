4f71a99d2a74005d22cf94735c19bab3f484eada
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-03-30 21:19:25 -0700

    render questions on submit button and render hints on hint button

--- JS/app.js
+++ JS/app.js
@@ -13,8 +13,7 @@ class Hints {
 }
 
 class Question {
-  constructor(number, question, answer, hint) {
-    this.number = number;
+  constructor(question, answer, hint) {
     this.question = question;
     this.answer = answer;
     this.hint = hint;
@@ -22,23 +21,90 @@ class Question {
   }
 }
 
-const myQuestion = new Question(
-  1,
-  "What is the original working title for E.T.?",
-  "A Boy's Life",
-  new Hints("Pellentesque habitant", "ultricies in", "adipiscing elit")
+const myQuestionOne = new Question(
+  "Who is the high school wise guy?",
+  "Ferris Bueller's",
+  new Hints("Day Off", "Matthew Broderick", "John Hughes Film")
 );
 
-const questionElement = document.getElementById("questionOne");
-questionElement.textContent = myQuestion.question;
+const myQuestionTwo = new Question(
+  "What year did Ferris Bueller's Day off came out?",
+  "1986",
+  new Hints("1984", "1986", "1985")
+);
 
-// let button = document.querySelector("button");
+const myQuestionThree = new Question(
+  "Ferris' sister's name was Jeanie, but according to her, her friends called her what?",
+  "Shaunna",
+  new Hints("Sandy", "Sarah", "Shaunna")
+);
 
-let button = document.querySelector("button[type='submit']");
+const myQuestionFour = new Question(
+  "Where did Ferris, Cameron, and Sloane take the day off to?",
+  "Chicago",
+  new Hints("Seattle", "Chicago", "New York City")
+);
 
-function Submit(e) {
+const myQuestionFive = new Question(
+  "What kind of car do the boys take out for the day?",
+  "Ferrari",
+  new Hints("Mustang", "Camaro", "Ferrari")
+);
+
+let arr = [
+  myQuestionOne,
+  myQuestionTwo,
+  myQuestionThree,
+  myQuestionFour,
+  myQuestionFive,
+];
+
+const questionElement = document.getElementById("question");
+questionElement.textContent = myQuestionOne.question;
+
+let hintBtn = document.querySelector("#hint");
+
+let submitBtn = document.querySelector("#submit");
+
+// let button = document.querySelector("button[type='submit']");
+
+function hint(e) {
   e.preventDefault();
-  console.log("clicked");
+  console.log("clicked hint");
+  let currentQuestionIndex = arr.findIndex(function (question) {
+    return question.question === questionElement.textContent;
+  });
+
+  let currentQuestion = arr[currentQuestionIndex];
+
+  let hintElement = document.getElementById("hint");
+
+  hintElement.textContent =
+    "Hint: " +
+    currentQuestion.hint.hint1 +
+    ", " +
+    currentQuestion.hint.hint2 +
+    ", " +
+    currentQuestion.hint.hint3;
 }
 
-button.addEventListener("click", Submit);
+function submit(e) {
+  e.preventDefault();
+  console.log("clicked submit");
+
+  let currentQuestionIndex = arr.findIndex(function (question) {
+    return question.question === questionElement.textContent;
+  });
+
+  let nextQuestionIndex = currentQuestionIndex + 1;
+
+  if (nextQuestionIndex < arr.length) {
+    let nextQuestion = arr[nextQuestionIndex];
+    questionElement.textContent = nextQuestion.question;
+  } else {
+    alert("Your done with the quiz!");
+  }
+}
+
+hintBtn.addEventListener("click", hint);
+submitBtn.addEventListener("click", submit);
