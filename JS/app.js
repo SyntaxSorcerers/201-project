47aac4c3f6de1a7ec405f6a0c71d820a735d8118
Lacey Washington <lacey.m.washington@gmail.com>
2023-04-01 11:20:41 -0500

    made 3 functions for comparing user answer and the start of a hint function that isn't quite done yet

--- JS/app.js
+++ JS/app.js
@@ -1,76 +1,127 @@
 
 'use strict';
+//helper array and var to hold questions used.
+let pocketArray = [];
+let currentQuestionInParr = pocketArray[pocketArray.length - 1];
+// state
 const state = {
   questions: [],
-  hints: [],
   score: 0,
 };
-const question = document.getElementById('question');
-const submitButton = document.getElementById('submit');
-const hintButton = document.getElementById('hint');
-const userInput = document.getElementById('user');
-const score = document.getElementById('scores');
-// help
+//DOM ref
+let question = document.getElementById('question');
+let submitButton = document.getElementById('submit');
+let hintButton = document.getElementById('hint');
+let userInputEvent = document.getElementById('current-question');
+let score = document.getElementById('scores');
+
+// helper function for generating a random question from the state index.
 function getRandomQuestion() {
   return Math.floor(Math.random() * state.questions.length);
 }
 
-state.prototype.renderQuestion = function () {
-  let pocketArray = [];
-  while (state.question.length){
-    let currentQuestion = getRandomQuestion();
-    question.textContent = state.questions[currentQuestion].question;
-    pocketArray.push(state.questions[currentQuestion]);
 
-    hintButton.onmouseover = function(){
-      while(state.questions[currentQuestion].attempts)
-      const ulelem = document.createElement('ul')
-        if(state.questions[currentQuestion].attempts === 3)
-          hintButton.textContent = state.questions[currentQuestion].hint[0];
-    } ifelse(state.questions[currentQuestion].attempts === 2){
-
-    }
-  }
-};
-
-
-
+//Hint helper constructor for question
 class Hints {
   constructor(hint1, hint2, hint3) {
-    this.hint1 = hint1;
-    this.hint2 = hint2;
-    this.hint3 = hint3;
-    state.hints.push(this);
+    this.hints = [hint1, hint2, hint3,];
   }
 }
 
 
-
+// question constructor function
 class Question {
-  constructor (question, answer, hint) {
+  constructor(question, answer, hint) {
     this.question = question;
     this.answer = answer;
     this.hint = hint;
-    this.attempts = 3;
+    this.attempts = 2;
     state.questions.push(this);
   }
-
-}
- function handleSubmit(event){
-
 }
 
+const myQuestion11 = new Question(
+  'What year was Forest Gump released?',
+  '1994',
+  new Hints('The Lion King was released in the same year', 'year', 'date')
+);
+
+const myQuestion12 = new Question(
+  'Which 90\'s movie featured Looney Tunes on it\'s soundtrack?',
+  'Space Jam',
+  new Hints('This movie also featured Michael Jordan', 'bugs bunny', 'stuff')
+);
+
+const myQuestion = new Question(
+  'What is the original working title for E.T.?',
+  `A Boy's Life`,
+  new Hints('Pellentesque habitant', 'ultricies in', 'adipiscing elit')
+);
+//Renders question and pushes the currentQuestion into a pocketArray
+//then it will remove the question from the state.questions so it will not
+//render again.
+function renderQuestion() {
+  let currentQuestion = getRandomQuestion();
+  question.innerText = state.questions[currentQuestion].question;
+  pocketArray.push(state.questions[currentQuestion]);
+  state.questions.splice(currentQuestion, 1);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let userInput = event.target.form.userInput.value;
+  console.log(userInput);
+  if (currentQuestionInParr.attempts > 0) {
+    if (userInput !== currentQuestionInParr.answer) {
+      currentQuestionInParr.attempts--;
+      console.log(currentQuestionInParr.attempts);
+      userInputEvent.reset();
+    } else {
+      state.score += 100;
+      score.textContent = state.score;
+      console.log(pocketArray);
+      renderQuestion();
+      userInputEvent.reset();
+    }
+  } else{
+    alert('out of attempts');
+    renderQuestion();
+    userInputEvent.reset();
+  }
+}
+function handleHints(event) {
+
+  const ulElem = document.createElement('ul');
+  hintButton.appendChild(ulElem);
+
+  if (state.questions[currentQuestion].attempts === 2){
+    let liElem = document.createElement('li');
+    liElem.textContent = state.questions[currentQuestion].hint[0];
+    ulElem.appendChild(liElem);
+  }
+  else if (state.questions[currentQuestion].attempts === 1) {
+    let liElem2 = document.createElement('li');
+    liElem2.textContent = state.questions[currentQuestion].hint[1];
+    ulElem.appendChild(liElem2);
+  }
+  else if (state.question[currentQuestion].attempts === 0) {
+    let liElem3 = document.createElement('li');
+    liElem3.textContent = state.questions[currentQuestion].hint[2];
+    ulElem.appendChild(liElem3);
+  }
+}
 console.log(state);
-submitButton.addEventListener('submit', handleSubmit);
+renderQuestion();
+submitButton.addEventListener('click', handleSubmit);
+hintButton.addEventListener('click', handleHints);
 
-
-// const myQuestion = new Question(
-//   1,
-//   "What is the original working title for E.T.?",
-//   "A Boy's Life",
-//   new Hints("Pellentesque habitant", "ultricies in", "adipiscing elit")
-// );
-
+// hintButton.onmouseenter();
+// let currentQuestion = getRandomQuestion();
+// console.log(state.questions[currentQuestion].attempts);
+// console.log(currentQuestion);
+// console.log(state.questions.length);
+// console.log(state);
+// renderQuestion();
 // const questionElement = document.getElementById("questionOne");
 // questionElement.textContent = myQuestion.question;
 
@@ -79,47 +130,11 @@ submitButton.addEventListener('submit', handleSubmit);
 // let button = document.querySelector("button[type='submit']");
 
 // function Submit(e) {
-//   e.preventDefault();
+  //   e.preventDefault();
 //   console.log("clicked");
 // }
 
 // button.addEventListener("click", Submit);
 
-
-
-// function question11() {
-//   let userInput = prompt('What year was Forest Gump released?');
-//   if (userInput === 1994){
-//     alert('Booyah! You got it!');
-//     console.log('Booyah! You got it!');
-//   } else if (userInput !== '1994'){
-//     alert('That answer was totally bogus, try again!');
-//   }
-// }
-// question11();
-
-// const myQuestion11 = new Question(
-//   'What year was Forest Gump released?',
-//   '1994',
-//   new Hints('The Lion King was released in the same year')
-// );
-// const questionElement11 = document.getElementById('questionEleven');
-// questionElement11.textContent = myQuestion11.question;
-// console.log(myQuestion11);
-
-// const myQuestion12 = new Question(
-//   'Which 90\'s movie featured Looney Tunes on it\'s soundtrack?',
-//   'Space Jam',
-//   new Hints('This movie also featured Michael Jordan')
-// );
-// const questionElement12 = document.getElementById('questionTwelve');
-// questionElement12.textContent = myQuestion12.question;
-// console.log(myQuestion12);
-
-// const myQuestion13 = new Question(
-//   'What movie is the quote "I see dead people" from?',
-//   'The Sixth Sense',
-//   new Hints('')
-// )
 
 
